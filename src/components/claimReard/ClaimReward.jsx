aced08381a6d9e92c9f41587cc4168e3dd92c30e
Saurabh patel <saurabhpatel5061456@gmail.com>
2024-04-16 09:04:50 +0530

    all error solved

--- src/components/claimReard/ClaimReward.jsx
+++ src/components/claimReard/ClaimReward.jsx
@@ -2,24 +2,38 @@
 import Web3Context from '@/context/Web3Context'
 import React, { useContext } from 'react'
 
+
+
 const ClaimReward = () => {
-    const{stakingContract}=useContext(Web3Context)
-
-    const fetchClaimReward=async()=>{
-        try {
-            const trasaction = await stakingContract.getReward();
-            // await trasaction.wait()
-            console.log("transactionnnn:",trasaction)
-
-        } catch (error) {
-            console.log("Claim Reward Failed",error)
-        }
-    }
+    const {stakingContract,selectedAccount}=useContext(Web3Context)
+ const claimReward = async()=>{
+  try{
+    const transaction = await stakingContract.getReward();
+    transaction.wait()
+    // await toast.promise(transaction.wait(),
+    // {
+    //   loading: "Transaction is pending...",
+    //   success: 'Transaction successful 👌',
+    //   error: 'Transaction failed 🤯'
+    // });
+    // if(receipt.status === 1){
+    //     setTransactionStatus("Transaction Is Successful")
+    //     setTimeout(()=>{
+    //       setTransactionStatus("")
+    //     },5000) 
+    //   } else{
+    //     setTransactionStatus("Transaction failed. Please try again.");
+    //   }
+  }catch(error){
+    // console.error("Claim Reward Failed",error.message)
+    console.log("err: claimReward:",error)
+  }
+ }
   return (
     <>
-    <div>
-        <button type='button' onClick={fetchClaimReward}>claim Reward</button>
-    </div>
+    <button  type="button" onClick={claimReward}>
+        claim reward
+    </button>
     </>
   )
 }
